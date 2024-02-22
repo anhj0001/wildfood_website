c43fa6c3bbdb7bdf1ae107f2c508863f68a2de30
Alberte <alan0002@stud.kea.dk>
2024-02-22 09:10:34 +0100

    java

--- js/productlist.js
+++ js/productlist.js
@@ -2,12 +2,28 @@
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb3R2Y2JhZGZ1dXRtdWF6eXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwNzk0NDIsImV4cCI6MjAyMzY1NTQ0Mn0.XEUaGYH4dOSjy_IarpkViFPDTlNJ4jiOwej4MMS8KkA
 
+/* 
 fetch("https://zjotvcbadfuutmuazywg.supabase.co/rest/v1/wildfood", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb3R2Y2JhZGZ1dXRtdWF6eXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwNzk0NDIsImV4cCI6MjAyMzY1NTQ0Mn0.XEUaGYH4dOSjy_IarpkViFPDTlNJ4jiOwej4MMS8KkA",
   },
+}) */
+
+const urlParams = new URLSearchParams(window.location.search);
+const mySeason = urlParams.get("season");
+console.log(mySeason)
+
+fetch(`https://zjotvcbadfuutmuazywg.supabase.co/rest/v1/wildfood?season=cs.["${mySeason}"]`,
+{
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb3R2Y2JhZGZ1dXRtdWF6eXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwNzk0NDIsImV4cCI6MjAyMzY1NTQ0Mn0.XEUaGYH4dOSjy_IarpkViFPDTlNJ4jiOwej4MMS8KkA",
+  },
 })
+
+
+
+
   .then((res) => res.json())
   .then(showItems);
 
@@ -23,6 +39,7 @@ function showItem(item) {
 
     copy.querySelector("h2").textContent = item.Name;
     copy.querySelector("img").src = item.prof_img;
+
     
 /*     copy.querySelector("p").textContent = item.Description;  */
 
